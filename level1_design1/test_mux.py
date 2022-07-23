f34a60205d5dda7a585edf4a37b2c301599d3a49
ANIMESH JANA <animesh.jana07@gmail.com>
2022-07-23 11:50:28 +0000

    updated

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,7 +6,7 @@ import random
 
 
 @cocotb.test()
-async def test_mux(dut):
+async def test_directed_mux(dut):
     """Test for mux2"""
     A = 12
     B =  2
@@ -17,6 +17,25 @@ async def test_mux(dut):
     dut.inp12.value = B
     dut.inp13.value = C
     await Timer(2, units='ns')
-
     assert dut.out.value == B, f"Adder result is incorrect: {dut.out.value} != B"
     #cocotb.log.info('##### CTB: Develop your test here ########')
+
+
+@cocotb.test()
+async def test_randomised_mux(dut):
+    """Test for adding 2 random numbers multiple times"""
+
+    for i in range(5):
+
+        A = random.randint(0, 31)
+        B = random.randint(0, 3)
+        C = random.randint(0, 3)
+
+        dut.sel.value = A
+        dut.inp12.value = B
+        dut.inp13.value = C
+        await Timer(2, units='ns')
+        
+        dut._log.info(f'A={A:05} B={B:05} model={A+B:05} DUT={int(dut.sum.value):05}')
+        assert dut.out.value == B, "Randomised test failed with: {B} != {OUT}".format(
+            A=dut.sel.value, B=dut.inp12.value, OUT=dut.out.value)
