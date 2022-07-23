0ada87e09885221ae0eea6fb5c3e8ae195f764f5
ANIMESH JANA <animesh.jana07@gmail.com>
2022-07-23 17:30:33 +0000

    updated

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -23,3 +23,23 @@ async def test_directed_mux(dut):
     assert dut.out.value == C, "MUX result is incorrect: {C} != {OUT}, expected value={EXP}".format(
             A=int(dut.sel.value), B=int(dut.inp12.value), C=int(dut.inp13.value) , OUT=int(dut.out.value), EXP=B)
 
+
+@cocotb.test()
+async def test_randomised_mux(dut):
+    """Test for 2 random numbers multiple times for inp12 and inp13 under select line 13"""
+
+    for i in range(5):
+        A = 13
+        B = random.randint(0, 3)
+        C = random.randint(0, 3)
+
+        dut.sel.value = A
+        dut.inp12.value = B
+        dut.inp13.value = C
+
+        await Timer(2, units='ns')
+        
+        dut._log.info(f'B={B:05} C={C:05}') 
+        #model={A+B:05} DUT={int(dut.out.value):05}')
+        assert dut.out.value == C, "Randomised test failed with: {C}! = {OUT}".format(
+            A=dut.sel.value, B=dut.b.value, OUT=dut.out.value)
