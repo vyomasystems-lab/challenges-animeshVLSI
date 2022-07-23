4fd1648dca0c9d6a7f2b709c6c30f4ef869ded42
ANIMESH JANA <animesh.jana07@gmail.com>
2022-07-23 13:30:13 +0000

    updated

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -17,5 +17,8 @@ async def test_directed_mux(dut):
     dut.inp12.value = B
     dut.inp13.value = C
     await Timer(2, units='ns')
-    assert dut.out.value == dut.inp12.value, f"Adder result is incorrect: {dut.out.value} != dut.inp12.value"
+    #assert dut.out.value == dut.inp12.value, f"Adder result is incorrect: {dut.out.value} != dut.inp12.value"
    # cocotb.log.info('##### CTB: Develop your test here ########')
+    assert dut.out.value == B, "MUX result is incorrect: {B} != {OUT}, expected value={EXP}".format(
+            A=int(dut.sel.value), B=int(dut.inp12.value), OUT=int(dut.out.value), EXP=B)
+
