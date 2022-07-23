997054286def0aed44f856b4b3f529a2a6a0a3c7
ANIMESH JANA <animesh.jana07@gmail.com>
2022-07-23 11:49:44 +0000

    updated

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,9 +2,21 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
+
 
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    A = 12
+    B =  2
+    C =  3
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    # input driving
+    dut.sel.value = A
+    dut.inp12.value = B
+    dut.inp13.value = C
+    await Timer(2, units='ns')
+
+    assert dut.out.value == B, f"Adder result is incorrect: {dut.out.value} != B"
+    #cocotb.log.info('##### CTB: Develop your test here ########')
